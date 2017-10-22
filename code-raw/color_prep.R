37090bb2736476ce42a9c6ead7a4115ed47d80ef
jn <tupiszakaczki@gmail.com>
2017-10-22 12:04:48 -0400

    nicer prep

--- code-raw/color_prep.R
+++ code-raw/color_prep.R
@@ -5,11 +5,12 @@ library(tidyverse)
 
 cc = fromJSON("code-raw/cartocolor.js") %>%
         map(t) %>%
-        map(as_data_frame) %>%
+        map(as.data.frame) %>%
         map_df(bind_rows, .id = "Name") %>%
         select(Name, tags, everything()) %>%
-        set_names(c("Name", "Tags", paste0("n", 2:11)))
+        set_names(c("Name", "Type", paste0("n", 2:11))) %>%
+        mutate(Type = replace(Type, Name == "Safe", "qualitative"))
 
 cc %>% View
 
-
+my_color = cc[cc$Name == "Burg", "n4"][[1]]
