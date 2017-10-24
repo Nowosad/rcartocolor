6bab2c2aade6a4be4f7439614e7072a032feb20b
jn <tupiszakaczki@gmail.com>
2017-10-23 21:26:06 -0400

    add metacartocolors docs

---
+++ R/metacartocolors.R
@@ -0,0 +1,11 @@
+#' CARTOColors database metadata
+#'
+#' A dataset containing the information about the CARTOcolor palettes.
+#' Source: https://github.com/CartoDB/CartoColor/blob/master/cartocolor.js.
+#'
+#' @format A data frame with 32 rows and 12 variables:
+#' * Name: palette name
+#' * Type: palette type
+#' * Min_n: minimal number of colors in a given palette
+#' * Max_n: maximal number of colors in a given palette
+"metacartocolors"
