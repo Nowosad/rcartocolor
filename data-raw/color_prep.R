4df6eff1ca025706ea1e4f707a07fba4988590c3
jn <tupiszakaczki@gmail.com>
2017-10-23 21:04:13 -0400

    cartocolors updated

--- data-raw/color_prep.R
+++ data-raw/color_prep.R
@@ -9,6 +9,6 @@ cartocolors = fromJSON("data-raw/cartocolor.js") %>%
         map_df(bind_rows, .id = "Name") %>%
         select(Name, tags, everything()) %>%
         set_names(c("Name", "Type", paste0("n", 2:11))) %>%
-        mutate(Type = replace(Type, Name == "Safe", "qualitative"))
+        mutate(Type = unlist(replace(Type, Name == "Safe", "qualitative")))
 
 devtools::use_data(cartocolors, overwrite = TRUE)
